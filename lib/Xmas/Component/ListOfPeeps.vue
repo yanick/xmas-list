2cc546d8f5fcfdbd0dd012029bc1313dbb3f202c
Yanick Champoux <yanick@babyl.dyndns.org>
2016-12-03 20:21:32 -0500

    list of peeps

---
+++ lib/Xmas/Component/ListOfPeeps.vue
@@ -0,0 +1,35 @@
+<template>
+<div>
+    <h2>Peeps</h2>
+    <ul>
+        <li><input placeholder="new giftee" v-model="name" @change="add_giftee" /></li>
+
+        <li v-for="giftee in giftees">
+            {{ giftee.name }}
+            <input type="button" value="remove" @click="remove_giftee( giftee.name )" />
+        </li>
+    </ul>
+</div> 
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    data: () => ({ name: '' }),
+    computed: {
+        ...mapState({
+            giftees: state => state.giftees
+        })
+    },
+    methods: {
+        add_giftee: function(value) {
+            this.$store.dispatch('add_giftee',value.target.value);
+            this.name = '';
+        },
+        remove_giftee: function(name) {
+            this.$store.dispatch('remove_giftee',name);
+        }
+    }
+};
+</script>
--- lib/Xmas/Component/Main.vue
+++ lib/Xmas/Component/Main.vue
@@ -2,16 +2,33 @@
     <div>
         <h1>Xmas list</h1>
 
-        <Budget :budget_left="budget_left" :budget_available="budget_available" />
+        <Budget />
+
+        <div v-if="budget_per_person">
+            <label>budget per person: {{ budget_per_person }} </label>
+        </div>
+
+        <ListOfPeeps />
 
     </div> 
 </template>
     
 <script>
-    import Budget from './Budget.vue';
+    import Budget      from './Budget.vue';
+    import ListOfPeeps from './ListOfPeeps.vue';
+
+    import { mapState, mapGetters } from 'vuex';
 
     export default {
-        components: { Budget },
-        data: () => ({ budget_left: 100, budget_available: 100 })
+        components: { Budget, ListOfPeeps },
+        computed: {
+            budget_per_person: function() {
+                if( ! this.nbr_giftees )  return false;
+
+                return this.budget_available / this.nbr_giftees;
+            },
+            ...mapGetters([ 'nbr_giftees' ]),
+            ...mapState({ budget_available: state => state.budget.available })
+        }
     };
 </script>
