27a97e090c09bc8b36c8a4f8cb854af8278fdff9
Yanick Champoux <yanick@babyl.dyndns.org>
2016-12-03 19:26:35 -0500

    budget

--- lib/Xmas/Component/Budget.vue
+++ lib/Xmas/Component/Budget.vue
@@ -1,15 +1,25 @@
 <template>
     <div>
         <label>Overall budget: 
-                <input type="number" :value="budget_available" />
+                <input type="number" v-model="budget_available" />
             </label>
             <div> Money left: {{ budget_left }} </div>
         </div>
 </template>
 
 <script>
+    import { mapState } from 'vuex';
+
     export default { 
-        props: [ 'budget_available', 'budget_left' ]
+        computed: {
+            budget_available: {
+                get()      { return this.$store.state.budget.available; },
+                set(value) { this.$store.dispatch( 'set_budget', value ) }
+            },
+            ...mapState({
+                budget_left: state => state.budget.left,
+            }),
+        }
     };
 </script>
 
